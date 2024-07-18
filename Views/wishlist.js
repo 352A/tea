a2f608c3b17cc4494965cfa49c43f69b8a62db6a
initmads <init.mads@gmail.com>
2024-07-18 09:18:31 +0300

    update

--- Views/wishlist.js
+++ Views/wishlist.js
@@ -1,12 +1,11 @@
 //render navbar
 UIController.renderNavbar();
+UIController.renderNavbar2();
 
 //welcome user
 UIController.welcomeUser("dark");
 
 // update cart icon notification
 UIController.updateCartIcon();
-
-// renderWishlist();
 
 UIController.renderWishlist();
